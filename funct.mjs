fdd2bdddf1d1d7b9df258725cf848eaceb481f4f
HASSANMEHMOOD17894 <hassanmehmood17894@gmail.com>
2024-11-30 21:34:38 +0500

    Functions

---
+++ funct.mjs
@@ -0,0 +1,57 @@
+
+// var year=+prompt("enter year");
+
+
+
+
+// function BOL( number){
+//     if(num===1){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+// }
+// isapplicable(age);
+// function isapplicable(age){
+//     if(age>=18){
+//         console.log("You are eligible to vote");
+//     }else{
+//         console.log("You are not eligible to vote");
+//     }
+// }
+
+
+// function leapyear(year){
+//     if(year%4===0){
+//         if(year%100===0){
+//             if(year%400===0){
+//                 console.log(year + " is a leap year");
+//             }else{
+//                 console.log(year + " is not a leap year");
+//             }
+//         } else{
+//             console.log(year + " is a leap year");
+//         }
+//     } else{
+//         console.log(year + " is not a leap year");
+//     }
+// }
+var vovel =prompt("enter an alphabet");
+chkvowel(vovel);
+function chkvowel(vovel){
+    if(vovel==='a'){
+        console.log("it's a vovel");
+    }
+    if(vovel==='e'){
+        console.log("it's a vovel");
+    }
+    if(vovel==='i'){
+        console.log("it's a vovel");
+    }
+    if(vovel==='o'){
+        console.log("it's a vovel");
+    }
+    if(vovel==='u'){
+        console.log("it's a vovel");
+    }
+}
